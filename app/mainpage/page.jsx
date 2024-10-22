116aa5f880982af4bf067aeef7926f87d9204f4b
Deekshigowda08 <149522963+Deekshigowda08@users.noreply.github.com>
2024-10-22 17:35:36 +0530

    mefnv

--- app/mainpage/page.jsx
+++ app/mainpage/page.jsx
@@ -1,40 +1,34 @@
-"use client";
+"use client"
 import Navbar from "../components/Navbar";
 import UserComp from "../components/UserComp";
 import Display from "../components/Display";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import jwt_decode from "jwt-decode"; 
-
+import { Suspense } from 'react';
+import { useEffect ,useState} from "react";
+import { jwtDecode } from "jwt-decode";
 export default function Home() {
-  const [username, setUsername] = useState(""); 
-  const [email, setEmail] = useState("");
-  const [id, setId] = useState("");
-  
-  const searchParams = useSearchParams();
-  const token = searchParams.get("token");
-
+  const [username, setSetUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [id, setid] = useState("")
+  const searchParams = useSearchParams()
+  const token = searchParams.get('token')
   useEffect(() => {
     if (token) {
-      try {
-        const decoded = jwt_decode(token, '@teamwe_08'); 
-        setUsername(decoded.username);
-        setEmail(decoded.email);
-        setId(decoded.objectid);
-      } catch (error) {
-        console.error("Invalid token", error);
-        window.location.replace('/'); 
-      }
-    } else {
-      window.location.replace('/'); 
+      const decoded = jwtDecode(token, '@teamwe_08'); 
+      setSetUsername(decoded.username);
+      setEmail(decoded.email);
+      setid(decoded.objectid)
+    }else{
+      window.location.replace(`/`)
     }
   }, [token]);
-
   return (
+    <Suspense fallback={<div>Loading...</div>}>
     <>
       <Navbar />
-      <UserComp username={username} email={email} />
+      <UserComp username={username} email={email}/>
       <Display id={id} />
     </>
+    </Suspense>
   );
 }
