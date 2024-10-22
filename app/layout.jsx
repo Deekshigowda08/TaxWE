16a7a2ddbf9db8e96818928d85174b1e2d539e26
Gagan-poojari <159212673+Gagan-poojari@users.noreply.github.com>
2024-10-22 17:38:57 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,8 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import { Suspense } from 'react';
+import { useSearchParams } from 'next/navigation';
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,8 +26,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        
-        {children}
+        <Suspense fallback={<div>Loading...</div>}>
+          {children}
+        </Suspense>
       </body>
     </html>
   );
